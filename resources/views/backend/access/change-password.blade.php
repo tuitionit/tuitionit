1791c9244d83d51a0395175d18bf7f1e64eb2844
Saranga Abeykoon <amisaranga@gmail.com>
2017-12-09 20:42:26 +0530

    User management screens UI fixes.

--- resources/views/backend/access/change-password.blade.php
+++ resources/views/backend/access/change-password.blade.php
@@ -12,47 +12,49 @@
 @section('content')
     {{ Form::open(['route' => ['admin.access.user.change-password', $user], 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'patch']) }}
 
+    <div class="col-lg-6 col-lg-offset-3">
         <div class="box box-success">
             <div class="box-header with-border">
                 <h3 class="box-title">{{ trans('labels.backend.access.users.change_password_for', ['user' => $user->name]) }}</h3>
 
                 <div class="box-tools pull-right">
-                    @include('backend.access.includes.partials.user-header-buttons')
-                </div><!--box-tools pull-right-->
+                    </div><!--box-tools pull-right-->
             </div><!-- /.box-header -->
 
             <div class="box-body">
                 <div class="form-group">
-                    {{ Form::label('password', trans('validation.attributes.backend.access.users.password'), ['class' => 'col-lg-2 control-label', 'placeholder' => trans('validation.attributes.backend.access.users.password')]) }}
+                    {{ Form::label('password', trans('validation.attributes.backend.access.users.password'), ['class' => 'col-md-4 control-label', 'placeholder' => trans('validation.attributes.backend.access.users.password')]) }}
 
-                    <div class="col-lg-10">
+                    <div class="col-md-8">
                         {{ Form::password('password', ['class' => 'form-control']) }}
                     </div><!--col-lg-10-->
                 </div><!--form control-->
 
                 <div class="form-group">
-                    {{ Form::label('password_confirmation', trans('validation.attributes.backend.access.users.password_confirmation'), ['class' => 'col-lg-2 control-label', 'placeholder' => trans('validation.attributes.backend.access.users.password_confirmation')]) }}
+                    {{ Form::label('password_confirmation', trans('validation.attributes.backend.access.users.password_confirmation'), ['class' => 'col-md-4 control-label', 'placeholder' => trans('validation.attributes.backend.access.users.password_confirmation')]) }}
 
-                    <div class="col-lg-10">
+                    <div class="col-md-8">
                         {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
                     </div><!--col-lg-10-->
                 </div><!--form control-->
             </div><!-- /.box-body -->
+
+            <div class="box-footer">
+                <div class="row">
+                    <div class="col-lg-11 col-lg-offset-1">
+                        <div class="row">
+                            <div class="col-xs-6 col-md-2 col-md-offset-6">
+                                {{ link_to_route('admin.access.user.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-default btn-block']) }}
+                            </div>
+                            <div class="col-xs-6 col-md-4">
+                                {{ Form::submit(trans('buttons.general.crud.update'), ['class' => 'btn btn-success btn-block']) }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- /.box-footer -->
         </div><!--box-->
-
-        <div class="box box-info">
-            <div class="box-body">
-                <div class="pull-left">
-                    {{ link_to_route('admin.access.user.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-danger btn-xs']) }}
-                </div><!--pull-left-->
-
-                <div class="pull-right">
-                    {{ Form::submit(trans('buttons.general.crud.update'), ['class' => 'btn btn-success btn-xs']) }}
-                </div><!--pull-right-->
-
-                <div class="clearfix"></div>
-            </div><!-- /.box-body -->
-        </div><!--box-->
+    </div>
 
     {{ Form::close() }}
 @endsection
